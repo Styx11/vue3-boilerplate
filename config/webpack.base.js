89e4274050f261b9797925677f44fc2b37ab0cb2
styx11 <1654630248@qq.com>
2020-10-27 12:49:57 +0800

    :wrench: update basic config

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -9,7 +9,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, '../dist/'),
 		filename: '[name].js',
-		publicPath: '/',
+		publicPath: '',
 	},
 	target: 'web',
 	module: {
@@ -19,8 +19,46 @@ module.exports = {
 				use: 'vue-loader'
 			},
 			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [
+					path.resolve(__dirname, '../src'),
+					path.resolve(__dirname, '../test'),
+				],
+			},
+			{
 				test: /\.css$/,
 				use: ['vue-style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-sprite-loader',
+				options: {
+					symbolId: 'icon-[name]'
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					name: 'static/img/[name].[hash:7].[ext]',
+				},
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: 'static/media/[name].[hash:7].[ext]',
+				},
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: 'static/fonts/[name].[hash:7].[ext]',
+				},
 			},
 		]
 	},
@@ -30,7 +68,7 @@ module.exports = {
 		// 抽取并 mini css 文件，注意它的 loader 应在 vue-loader 之后
 		// 因为 extractcss 插件存在接口使用错误，所以使用这个插件
 		new MiniCssExtractPlugin({
-			filename: '/static/css/[name].[contenthash].css',
+			filename: 'static/css/[name].[contenthash].css',
 		}),
 	]
 }
