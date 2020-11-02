bc72f525a1391a0993637471f8f3b77660b779a1
styx11 <1654630248@qq.com>
2020-11-02 18:54:40 +0800

    update app with vuex

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 
 // 创建一个独立的根实例，防止全局污染
 const app = createApp(App)
 
 app
+	.use(store)
 	.use(router)
 	.mount('#app')
